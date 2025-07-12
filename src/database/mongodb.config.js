06f088a4d69c2effd93409233b6fb6f8968c4b64
ishto05 <justvirtualid404@gmail.com>
2025-07-12 18:27:29 +0530

    configured database connection MONGO/SQL

---
+++ src/database/mongodb.config.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env.js";
+
+if (!DB_URI) {
+  console.error("No Database URI found!");
+  process.exit(1);
+}
+
+const connectDB = async () => {
+  console.log("Initializing connection to MongoDB....");
+
+  try {
+    const options = {
+      serverSelectionTimeoutMS: 5000,
+      family: 4,
+    };
+
+    mongoose.connection.on("error", (err) => {
+      console.error("❌ MongoDB connection error:", err);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.warn("❌ MongoDB disconnected. Attempting to reconnect...");
+    });
+
+    mongoose.connection.on("connected", () => {
+      console.log("✅ MongoDB connection established");
+    });
+
+    mongoose.connection.on("reconnected", () => {
+      console.log("✅ MongoDB reconnected");
+    });
+
+    if (NODE_ENV === "Development") {
+      //debug mode
+      mongoose.set("debug", true);
+    }
+
+    await mongoose.connect(DB_URI, options);
+    console.log(`✅ MongoDB connected in --${NODE_ENV}-- mode`);
+  } catch (error) {
+    console.error("❌ Failed to connect to database:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/database/sqldb.config.js
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize";
+import { SQL } from "../config/env.js";
+
+const sequelize = new Sequelize(SQL.DATABASE_SQL, SQL.USER_SQL, SQL.PASSWD_SQL, {
+  host: SQL.HOST_SQL,
+  dialect: "mysql",
+});
+
+sequelize.authenticate()
+  .then(() => console.log("MySQL connected.."))
+  .catch((err) => console.log("MySQL err: ", err));
+
+export default sequelize;
