a6118c1581c3243292aa856880bed8d92dcb8414
ishto05 <justvirtualid404@gmail.com>
2025-07-12 18:25:57 +0530

    configured env.js

---
+++ src/config/env.js
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+
+  JWT_SECRET,
+  JWT_EXPIRY,
+} = process.env;
+
+export const SQL = {
+  HOST_SQL: process.env.HOST_SQL,
+  USER_SQL: process.env.USER_SQL,
+  PASSWD_SQL: process.env.PASSWD_SQL,
+  DATABASE_SQL: process.env.DATABASE_SQL,
+};
